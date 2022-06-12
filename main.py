6350d03db0eb1268130a60e14872962e4ffde4b5
kunjir9542 <73615796+kunjir9542@users.noreply.github.com>
2022-06-12 15:54:38 -0500

    Add files via upload

---
+++ main.py
@@ -0,0 +1,16 @@
+import random
+from math import trunc
+from random import randint
+import time
+import keyboard
+
+dadJokes = []
+
+
+def titleScreen():
+    choice = input("How much sanitizer does it take to kill a person?")
+    print("Don't drink hand sanitizer kids.")
+
+
+
+titleScreen()
